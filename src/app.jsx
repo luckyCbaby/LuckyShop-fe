1e967c4ed359ee1bc924c0e9e30902c45d43ef47
shijie <sj95516@163.com>
2019-01-29 17:17:10 +0800

    初始化

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: shijie
+ * @Date:   2019-01-29 12:28:46
+ * @Last Modified by:   shijie
+ * @Last Modified time: 2019-01-29 13:13:24
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+import './index.css';
+import './index.scss';
+
+ReactDOM.render(
+	<div>
+		<i className="fa fa-address-book"></i>
+		<h1>hello shijie</h1>
+	</div>,
+	document.querySelector('#app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+/*
+ * @Author: shijie
+ * @Date:   2019-01-29 12:26:47
+ * @Last Modified by:   shijie
+ * @Last Modified time: 2019-01-29 17:00:07
+ */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+module.exports = {
+	entry: './src/app.jsx',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: '/dist/',
+		filename: 'js/app.js'
+	},
+	module: {
+		rules: [
+			//react语法处理
+			{
+				test: /\.jsx$/,
+				exclude: /(node_modules)/, //这个目录下的文件不做处理
+				use: {
+					loader: 'babel-loader',
+					options: {
+						//env即environment，自动根据环境打包，不管是浏览器环境还是node环境
+						presets: ['env', 'react']
+					}
+				}
+			},
+			//css文件处理
+			{
+				test: /\.css$/,
+				use: ExtractTextWebpackPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader'
+				})
+			},
+			//sass文件处理
+			{
+				test: /\.scss$/,
+				use: ExtractTextWebpackPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			},
+			//图片及静态文件处理
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 8192,
+						name: 'resource/[name].[ext]' //指定图片打包路径，[ext]表示它是什么后缀导出什么后缀
+					}
+				}
+			},
+			//字体图标的处理,和图片的配置一样
+			{
+				test: /\.(woff|woff2|eot|svg|ttf|tof)$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 8192,
+						name: 'resource/[name].[ext]'
+					}
+				}
+			}
+		]
+	},
+	//配置webpack-dev-server
+	devServer: {
+		contentBase: './dist',
+		port: 8020,
+		open: true, //执行webpack-dev-server自动打开浏览器
+
+	},
+	plugins: [
+		//提出html文件
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html' //打包后的文件
+		}),
+		//独立css文件
+		new ExtractTextWebpackPlugin('css/[name].css'),
+		//提出公共模块
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'common', //name属性是手动指定的公共模块
+			filename: 'js/base.js' //公共模块打包到指定文件
+
+		})
+	]
+}
