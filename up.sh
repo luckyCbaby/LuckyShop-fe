5620e6fbe4bafe0402ed95365767563e13ccab97
dongcewei <dongcewei@baidu.com>
2019-02-17 01:33:57 +0800

    up.sh

--- up.sh
+++ up.sh
@@ -1,25 +1,44 @@
-echo ""###################开始###################""
+GIT_HOME=/developer/git-repository/
+DEST_PATH=/product/front/
 
-	git pull
-	
-echo ""###################切换分支###################""
+if [ ! -n "$1" ];
+then
+    echo -e "Please input a project name! You can input as follows:"
+    echo -e "./fe-deploy.sh admin-v2-fe"
+    exit
+fi
 
-	git checkout master
+if [ $1 = "mmall_admin_fe" ];
+then
+    echo -e "---------Enter Project--------"
+    cd $GIT_HOME$1
+else
+    echo -e "Invalid Project Name!"
+    exit
+fi
 
-echo ""###################安装依赖###################""
+# clean dist
+echo -e "---------Clean Dist--------"
+rm -rf ./dist
 
-	npm i
+echo -e "---------Git Pull--------"
+git pull
 
-echo ""####################打包文件##################""
+echo -e "---------Npm Install--------"
+npm i
 
-	npm run dist
+echo -e "---------Npm Run Dist--------"
+npm run dist
 
-echo ""###################移除dist###################""
-	
-	rm -rf /product/dist
-	
-echo ""###################移动dist###################""
-	
-	mv dist /product/
+if [ -d "./dist" ];
+then
+    echo -e "---------clean Dist--------"
+    rm -rf $DEST_PATH/dist
 
-echo ""###################完成###################""
+    echo -e "---------copy Dist--------"
+    cp -R ./dist $DEST_PATH/$1/
+
+    echo -e "---------Deploy Success--------"
+else
+    echo -e "---------Deploy Fail--------"
+fi
